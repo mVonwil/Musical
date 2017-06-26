3fab42a42d01176ca572487398195bdd0fce25c7
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-06-26 11:26:28 +1000

    Beginning of GreyBox. Road is working.

---
+++ Assets/Road.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += new Vector3 (0, 0, -0.25f);
+	}
+}
---
+++ Assets/StartCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCollider : MonoBehaviour {
+
+	public GameObject sampleRoad;
+
+	void OnTriggerEnter(Collider car){
+		if (car.gameObject.tag == "Player")
+			Instantiate(sampleRoad, new Vector3(0, 0, 46), Quaternion.identity);
+	}
+}
