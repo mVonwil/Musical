5458ef49fcc2723de663bfb00e0ae4488f921f79
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-06-28 18:09:30 +1000

    Coded the third transition (Rising Road).

--- Assets/RisingCar.cs
+++ Assets/RisingCar.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RisingCar : MonoBehaviour {
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		transform.position += (Vector3.forward / 5);
 		transform.position += (Vector3.up / 50);
 	}
---
+++ Assets/RisingRoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingRoad : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//transform.localPosition += (transform.forward / 2);
+		//rb.AddForce (transform.forward * 50);
+		rb.MovePosition(transform.position + (Vector3.forward + transform.forward).normalized / 3);
+	}
+}
