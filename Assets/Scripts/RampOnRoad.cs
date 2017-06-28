d8db1c7e9ec6382d01fe951e8c906c37f532efc9
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-06-28 20:50:21 +1000

    Making adjustments to the transitions.

--- Assets/Scripts/RampOnRoad.cs
+++ Assets/Scripts/RampOnRoad.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class RampOnRoad : MonoBehaviour {
 
 	public float speed = 0.25f;
-	public Quaternion playerRot;
-
-	void Start(){
-		playerRot = transform.rotation;
-	}
 
 	void FixedUpdate(){
 		transform.position += (Vector3.forward * speed);
--- Assets/Scripts/RisingRoad.cs
+++ Assets/Scripts/RisingRoad.cs
@@ -6,9 +6,18 @@ public class RisingRoad : MonoBehaviour {
 
 	public Rigidbody rb;
 	public float speed = 0.3f;
+	public Quaternion endRot;
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		rb.MovePosition(transform.position + (Vector3.forward + transform.forward).normalized * speed);
+	}
+
+	void OnTriggerEnter(Collider endGate){
+		if (endGate.gameObject.tag == "Finish") {
+			endRot = transform.rotation;
+			endRot.x = (-90);
+			transform.rotation = endRot;
+		}
 	}
 }
