1c07196458f53878017953872f39163958e9a13e
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-06-28 17:36:31 +1000

    Coded the second transition (Ramp on Road).

---
+++ Assets/RampOnRoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampOnRoad : MonoBehaviour {
+
+	public Quaternion playerRot;
+
+	void Start(){
+		playerRot = transform.rotation;
+	}
+
+	void FixedUpdate(){
+		transform.position += (Vector3.forward / 5);
+	}
+}
