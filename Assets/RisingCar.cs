5c8eb98cc842b13dd3a23e5a1555ece11b7b21bd
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-06-28 16:41:16 +1000

    Coded the first transition (Rising Car).

---
+++ Assets/RisingCar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingCar : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += (Vector3.forward / 5);
+		transform.position += (Vector3.up / 50);
+	}
+}
