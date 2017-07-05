18847083d01535106539f9ec8e30e63a4f2013c6
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-07-05 17:54:54 +1000

    Made the player prefab for space. Includes movement scripting.

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour {
+
+	public Vector3 camTrans;
+	public Transform playerTrans;
+	public Vector3 camOffset;
+
+	// Use this for initialization
+	void Start () {
+		camTrans = transform.position;
+		camOffset = new Vector3 (0, 0.92f, -3.204f);
+		playerTrans = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camTrans = playerTrans.position + camOffset;
+		transform.position = camTrans;
+	}
+}
---
+++ Assets/Scripts/SpaceMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMovement : MonoBehaviour {
+
+	public Quaternion playerRot;
+	public GameObject parentObj;
+	public Vector3 parentPos;
+	public float RotSpeed;
+	public float slideSpeed;
+	public int slideLimit;
+
+	// Use this for initialization
+	void Start () {
+		playerRot = transform.rotation;
+		parentObj = GameObject.FindGameObjectWithTag ("PlayerMover");
+		parentPos = parentObj.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		PlayerRotate ();
+		PlayerSlide ();
+	}
+
+	void PlayerRotate(){
+		if (Input.GetKey (KeyCode.A)) {
+			playerRot.z += RotSpeed * Time.deltaTime;
+			transform.Rotate (new Vector3 (0, 0, RotSpeed * Time.deltaTime));
+		} else if (Input.GetKey (KeyCode.D)) {
+			playerRot.z -= RotSpeed * Time.deltaTime;
+			transform.Rotate (new Vector3 (0, 0, -RotSpeed * Time.deltaTime));
+		}
+	}
+
+	void PlayerSlide(){
+		if (Input.GetKey (KeyCode.D)) {
+			parentPos.x += slideSpeed * Time.deltaTime;
+			parentObj.transform.localPosition = parentPos;
+			if (parentPos.x >= slideLimit)
+				parentPos.x = slideLimit;
+		} else if (Input.GetKey (KeyCode.A)) {
+			parentPos.x -= slideSpeed * Time.deltaTime;
+			parentObj.transform.localPosition = parentPos;
+			if (parentPos.x <= -slideLimit)
+				parentPos.x = -slideLimit;
+		}
+	}
+}
