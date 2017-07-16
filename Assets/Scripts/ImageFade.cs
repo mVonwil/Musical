cec4e49fef8520134bbe7b51ca3d85c57ad7f254
Matthew von Wilpert <1004624@student.sae.edu.au>
2017-07-17 01:18:58 +1000

    Commiting work done at home. Added car.

---
+++ Assets/Scripts/ImageFade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageFade : MonoBehaviour {
+
+	public CanvasGroup canvas;
+	public float time2Fade = 2;
+	
+	// Update is called once per frame
+	void Update () {
+		canvas.alpha -= (Time.deltaTime / time2Fade);
+	}
+}
---
+++ Assets/Scripts/RoadMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMovement : MonoBehaviour {
+
+	public GameObject parentObj;
+	public Vector3 parentPos;
+	public float slideSpeed;
+	public int slideLimit;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		parentObj = GameObject.FindGameObjectWithTag ("PlayerMover");
+		parentPos = parentObj.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		PlayerSlide ();
+	}
+
+	void PlayerSlide(){
+		if (Input.GetKey (KeyCode.D)) {
+			parentPos.x += slideSpeed * Time.deltaTime;
+			parentObj.transform.localPosition = parentPos;
+			if (parentPos.x >= slideLimit)
+				parentPos.x = slideLimit;
+		} else if (Input.GetKey (KeyCode.A)) {
+			parentPos.x -= slideSpeed * Time.deltaTime;
+			parentObj.transform.localPosition = parentPos;
+			if (parentPos.x <= -slideLimit)
+				parentPos.x = -slideLimit;
+		}
+	}
+}
